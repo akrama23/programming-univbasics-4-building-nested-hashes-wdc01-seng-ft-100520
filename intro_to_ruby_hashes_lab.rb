064aabddff28042fd0522e4d33077f3e74bf104b
akam alam <arundelfootball23@gmail.com>
2020-09-24 23:52:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,8 +8,9 @@ end
 
 def monopoly_with_second_tier
   
+   monolopy = {:railroads => {}
    
-  bash_hash[:railroads] =[ {:pieces => 4 }, {:rent_in_dollars => {} }, 
+  base_hash[:railroads] =[ {:pieces => 4 }, {:rent_in_dollars => {} }, 
 {:names => {} } ]
   
 
