8cdbb7e8cd66feabc50b124fc84fa5b5e993d5c0
akam alam <arundelfootball23@gmail.com>
2020-09-24 23:19:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,6 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-pieces = 4
 base_hash[:railroads]= [ {:pieces }, {:rent_in_dollars => {} }, 
 {:names => {} } ]
   
