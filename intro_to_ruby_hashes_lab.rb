bce2e30014261c493b147e4c8dfc9514504d0439
akam alam <arundelfootball23@gmail.com>
2020-09-25 00:18:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,11 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 
- 
+ monolopy = {:railroads => {
+     pieces: 4,
+     rent_in_dollars: {},
+     names: {}
+     
  
 end
 
