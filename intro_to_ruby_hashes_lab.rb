4ca1f21cb61da7736872543332233001585d8608
akam alam <arundelfootball23@gmail.com>
2020-09-25 01:06:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ monolopy = {:railroads => {
     three_pieces_owned: 100,
     four_pieces_owned: 200
     },
-    
+    names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {}}}}
 end
 
 def monopoly_with_fourth_tier
