92fad10fe49635d2c3f78ea885aec3646728d797
akam alam <arundelfootball23@gmail.com>
2020-09-25 00:34:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,16 +18,18 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
+  
+  monopoly_with_second_tier[:railroads][1]= 
  
- monolopy = {:railroads => {
-     pieces: 4,
-     rent_in_dollars: {one_piece_owned: 25, 
-     two_pieces_owned: 50,
-     three_pieces_owned: 100,
-     four_pieces_owned: 200
-     },
-     names: {}
- }
+# monolopy = {:railroads => {
+#     pieces: 4,
+#     rent_in_dollars: {one_piece_owned: 25, 
+#     two_pieces_owned: 50,
+#     three_pieces_owned: 100,
+#     four_pieces_owned: 200
+#     },
+#     names: {}
+# }
 end
 
 # def monopoly_with_fourth_tier
